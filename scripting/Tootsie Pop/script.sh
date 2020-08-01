7e587cf7161c98db93760ec7a0927864fc89ae31
Shubham Kumar <sshubhamk1@hotmail.com>
2020-08-01 16:08:45 +0530

    Tootsie Pop

---
+++ scripting/Tootsie Pop/script.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+while  [ 1 ]
+do
+	for i in $(ls)
+	do
+		if [[ $(file -ib $i | cut -d ';' -f 1) == "application/gzip" ]]
+		then
+			if  [[ $i == *.gz ]]
+			then
+				gunzip -q $i;
+
+			else
+				mv $i $i.gz;
+				echo "removing by adding extension";
+				gunzip -q $i;
+			fi
+		fi
+		if [[ $(file -ib $i | cut -d ';' -f 1) == "application/x-xz" ]]
+		then
+			if  [[ $i == *.xz ]]
+			then
+				unxz $i && rm $i;
+
+			else
+				mv $i $i.xz;
+				echo "removing by adding extension";
+				unxz $i && rm $i;
+			fi
+		fi
+		if [[ $(file -ib $i | cut -d ';' -f 1) == "application/zip" ]]
+		then
+			if  [[ $i == *.zip ]]
+			then
+				unzip -q $i && rm $i;
+
+			else
+				mv $i $i.zip;
+				echo "removing by adding extension";
+				unzip -q $i && rm $i;
+			fi
+		fi
+		if [[ $(file -ib $i | cut -d ';' -f 1) == "application/x-bzip2" ]]
+		then
+			if  [[ $i == *.bz2 ]]
+			then
+				bzip2 -d $i;
+
+			else
+				mv $i $i.bz2;
+				echo "removing by adding extension";
+				bzip2 -d $i;
+			fi
+		fi
+	done
+done
+# Although this worked but elegant solution would be to use 7z instead of seperate tools for every one
